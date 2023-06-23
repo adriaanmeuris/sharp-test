f26a9951f866ecc675a4365fe6ee6faf1eafb7a1
Adriaan Meuris <adriaan.meuris@gmail.com>
2023-06-23 09:18:53 +0200

    update code to verify sha256 checksum

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream, writeFile } = require('fs/promises');
+const { createHash } = require('node:crypto');
 const sharp = require('sharp');
+
+const simdEnabled = true;
+sharp.simd(simdEnabled);
+
+const EXPECTED_CHECKSUM = 'b2e6910051b05c7bcbb9ec1bacb62dfad77ed95c208500f66d7edfdff5056675';
+const EXPECTED_CHECKSUM_NO_SIMD = '8543676fc6304ac7860a9c85a8a4a63fbca561681c57a4ae09fcbd67ca251576';
+
+function sha256(content) {
+  return createHash('sha256').update(content).digest('hex');
+}
 
 async function createThumbWithPipeline(outputPath) {
   const sharpOperations = sharp({
@@ -35,9 +46,30 @@ async function createThumb(outputPath) {
   console.log(`created thumb "${outputPath}"`);
 }
 
+async function createThumbWithChecksum(outputPath) {
+  // Generate thumb
+  const buf = await sharp('./base.png', {
+    failOn: 'none',
+    limitInputPixels: false,
+  })
+    .flatten({ background: { r: 255, g: 255, b: 255 } })
+    .resize({ width: 496, height: 702 })
+    .jpeg({ quality: parseInt(80, 10) })
+    .toBuffer();
+
+  // Write to disk if checksum doesn't match
+  const checksum = simdEnabled ? EXPECTED_CHECKSUM : EXPECTED_CHECKSUM_NO_SIMD;
+  if (sha256(buf) !== checksum) {
+    console.log(`  ${outputPath}: issue with generation, saved to disk`);
+    await writeFile(outputPath, buf);
+  } else {
+    console.log(`  ${outputPath}: generated correctly`);
+  }
+}
+
 (async function app() {
-  const amt = new Array(100).fill(undefined);
-  const generations = amt.map((_, idx) => createThumb(`/outputs/base-thumb-${idx}.jpg`));
+  const amt = new Array(500).fill(undefined);
+  const generations = amt.map((_, idx) => createThumbWithChecksum(`/outputs/base-thumb-${idx.toString().padStart(5, '0')}.jpg`));
   await Promise.all(generations);
 
   console.log('done');
